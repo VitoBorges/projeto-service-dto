349bfc79ad886170c993301a14a26be20a36476d
VitoBorges <vitorab@gmail.com>
2021-11-05 12:00:21 -0300

    Nosso primeiro commit - Criação do Projeto

---
+++ src/test/java/br/org/serratec/end/ProjetoServiceDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoServiceDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
